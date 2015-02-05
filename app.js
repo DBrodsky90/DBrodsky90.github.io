2cee4f530a4d2ca476eed71b3a221cc0c15b2f20
DBrodsky90 <DBrodsky90@gmail.com>
2015-02-05 16:09:06 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['About', 'Projects', 'Contact'],
+		sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+		slidesNavigation: true,
+	});
+});
